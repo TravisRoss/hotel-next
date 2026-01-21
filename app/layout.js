35d5102e3a2028b6d91a46081a0599f411c7c012
Travis Ross <travis.ross@gira.de>
2026-01-21 20:19:42 +0100

    chore: add some styling with tailwind css

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import Navigation from "./_components/navigation";
 
+import "@/app/_styles/globals.css";
+
 export const metadata = {
   title: "First Next.js app",
 };
@@ -7,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-primary-950 text-primary-100 min-h-screen">
         <Navigation />
         <main>{children}</main>
         <footer>some footer info</footer>
