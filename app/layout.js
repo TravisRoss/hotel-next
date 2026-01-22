4e2cc6840a44f444229cb6f4be98b4c553e679f9
Travis Ross <travis.ross@gira.de>
2026-01-22 20:17:37 +0100

    chore: add image optimisation using Next.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,14 @@
 import Navigation from "./_components/navigation";
 
 import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
+
+const josefinSans = Josefin_Sans({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-josefin-sans",
+});
 
 export const metadata = {
   title: {
@@ -13,10 +21,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
-        <Navigation />
-        <main>{children}</main>
-        <footer>some footer info</footer>
+      <body
+        className={`${josefinSans.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+      >
+        <Header />
+
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl bg-red-500 mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
